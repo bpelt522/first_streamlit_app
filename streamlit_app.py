ca08313e6f235b1c4035922f7553dc0a5176419f
bpelt522 <159646066+bpelt522@users.noreply.github.com>
2024-02-11 23:36:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/(fruityvice_response)")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/'fruityvice_response'")
 
 
 # write your own comment -what does the next line do? 
